8f04f6d0025ee8b204d54eb8a4decdc84d90b110
Hà Vũ Long <longmoon2004@gmail.com>
2025-09-26 04:20:03 +0700

    Delete su25/src/main/java/banhangrong/su25/repository directory

--- su25/src/main/java/banhangrong/su25/repository/UsersRepository.java
+++
@@ -1,8 +0,0 @@
-package banhangrong.su25.Repository;
-
-import banhangrong.su25.Entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, Long> {
-    // Có thể thêm các phương thức truy vấn tuỳ ý ở đây
-}
