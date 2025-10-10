6dd8ed1606a517c5457c9e504cf421bf436f0917
dat dang <2imdanton139@gmail.com>
2025-10-10 14:16:47 +0700

    chore: startup link on actual port; default back to 8080

--- src/main/java/banhangrong/su25/Su25Application.java
+++ src/main/java/banhangrong/su25/Su25Application.java
@@ -2,10 +2,16 @@ package banhangrong.su25;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class Su25Application {
     public static void main(String[] args) {
-        SpringApplication.run(Su25Application.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(Su25Application.class, args);
+        Environment env = ctx.getEnvironment();
+        String port = env.getProperty("local.server.port", env.getProperty("server.port", "8080"));
+        String url = "http://localhost:" + port ;
+        System.out.println("Enter to star: " + url);
     }
 }
