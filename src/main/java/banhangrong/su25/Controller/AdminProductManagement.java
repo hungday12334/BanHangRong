d77f583ae64455b0c49198518efb09bd786ba0d6
hungqt9x78 <nthung06082k5@gmail.com>
2025-10-30 09:03:26 +0700

    feat(product): add filter fuction controller

--- src/main/java/banhangrong/su25/Controller/AdminProductManagement.java
+++ src/main/java/banhangrong/su25/Controller/AdminProductManagement.java
@@ -1,5 +1,6 @@
 package banhangrong.su25.Controller;
 
+import banhangrong.su25.DTO.ProductFilter;
 import banhangrong.su25.Entity.Products;
 import banhangrong.su25.service.AdminProductService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,9 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin/product")
@@ -17,6 +21,14 @@ public class AdminProductManagement {
     @Autowired
     AdminProductService adminProductService;
 
+
+    @GetMapping("/filter")
+    public String filterProduct(@ModelAttribute("filter")ProductFilter productFilter, RedirectAttributes redirectAttributes) {
+        List<Products> lisFilterProduct = adminProductService.filter(productFilter);
+        redirectAttributes.addFlashAttribute("filter", lisFilterProduct);
+        redirectAttributes.addFlashAttribute("isFromFilter", true);
+        return "redirect:/admin/products";
+    }
     @GetMapping("/update")
     public String showUpdateForm(HttpServletRequest request,
                                  Model model,
